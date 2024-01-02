84426082e5dd41c974a65af0c4e4869ea0a2d1a3
시모니(Simoni) <145527459+simoni-git@users.noreply.github.com>
2024-01-02 21:21:55 +0900

    앱 공식 배포 1.0
    앱 공식배포 1.0
    달라진것: 권한허용멘트때문에 리젝먹음, 그래서 수정후 배포함

--- ZesulE/AppDelegate.swift
+++ ZesulE/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         NMFAuthManager.shared().clientId = "wz4h8i23k6"
+        sleep(3)
         return true
     }
 
